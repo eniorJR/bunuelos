0e60c76f39af017556c6e392c264d98c0445b6ac
uriimarquess <marquesnooriok@gmail.com>
2025-04-04 00:55:25 +0200

    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "server",
 
+  output: "server",
   integrations: [react()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
